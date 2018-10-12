a4a6fcc62098570b91a952e188d72d84a9aa1427
qianhc <953995648@qq.com>
2018-10-12 17:45:03 +0800

    Initial Commit

---
+++ JNWaterFallLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JNWaterFallLayout
+//
+//  Created by 钱海超 on 2018/10/12.
+//  Copyright © 2018年 北京大账房信息技术有限公司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
