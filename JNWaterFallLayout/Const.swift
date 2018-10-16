a870673a7a967c3298f55bff3d02018787c784b8
qianhc <953995648@qq.com>
2018-10-16 15:33:31 +0800

    自定义瀑布流

---
+++ JNWaterFallLayout/Const.swift
@@ -0,0 +1,17 @@
+//
+//  Const.swift
+//  JNWaterFallLayout
+//
+//  Created by 钱海超 on 2018/10/12.
+//  Copyright © 2018年 北京大账房信息技术有限公司. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+
+let kScreenW = UIScreen.main.bounds.width
+let kScreenH = UIScreen.main.bounds.height
+
+let kNavigationBarH : CGFloat = 44
+let kStatusBarH : CGFloat = 20
--- JNWaterFallLayout/ViewController.swift
+++ JNWaterFallLayout/ViewController.swift
@@ -10,16 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    fileprivate lazy var collectionView: UICollectionView = {
+
+        let layout = JNWaterFlowLayout()
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        layout.dataSource = self
+        layout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10)
+
+        let collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
+//        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+
+        return collectionView
+
+    }()
+
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.addSubview(collectionView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension ViewController : JNWaterFlowLayoutDataSource {
+    func numberOfColsInWaterFlowLayout(_ layout: JNWaterFlowLayout) -> Int {
+        return 3
+    }
+    func waterFlowLayout(_ layout: JNWaterFlowLayout, indexPath: IndexPath) -> CGFloat {
+        return indexPath.item % 2 == 0 ? kScreenW * 2 / 3 : kScreenW * 0.5
+    }
+}
+
+
+extension ViewController : UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
     }
 
-
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.contentView.backgroundColor = UIColor.gray
+        return cell
+    }
 }
 
